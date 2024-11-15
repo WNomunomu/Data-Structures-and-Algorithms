4eddf7a82d81d8cf66b7c1207079a9a1b1320cd9
WNomunomu <kohsei.nmr@gmail.com>
2024-11-15 16:18:55 +0900

    create the tree data structure

---
+++ 8.tree/rooted_trees.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+
+#define MAX 100005
+#define NIL -1
+#define rep(i, n) for (int i = 0; (i) < (n); ++(i))
+
+struct Node { int parent, leftChild, rightSibling; };
+
+Node Tree[MAX];
+int n, Depth[MAX];
+
+void printChildlen(int parentNodeId) {
+  Node parentNode = Tree[parentNodeId];
+  cout << "[";
+  Node node = Tree[parentNode.leftChild];
+  // int rightSiblingId;
+  for (int i = 0; node.rightSibling != NIL; i++) {
+    if (i == 0) {
+      if (parentNode.leftChild != NIL) {
+        cout << parentNode.leftChild;
+      }
+      node = Tree[parentNode.leftChild];
+    }
+    else {
+      cout << ", " << node.rightSibling;
+      node = Tree[node.rightSibling];
+    }
+  }
+  cout << "]";
+}
+
+void printNode(int id) {
+  Node node = Tree[id];
+  int depth = Depth[id];
+  cout << "node " << id << ": ";
+  cout << "parent = " << node.parent << ", ";
+  cout << "depth = " << depth << ", ";
+  if (node.parent == NIL) {
+    cout << "root, ";
+  }
+  else if (node.leftChild == NIL) {
+    cout << "leaf, ";
+  }
+  else {
+    cout << "internal node, ";
+  }
+  printChildlen(id);
+  cout << "\n";
+
+}
+
+void setDepth(int nodeId, int depth) {
+  Depth[nodeId] = depth;
+  if (Tree[nodeId].rightSibling != NIL) setDepth(Tree[nodeId].rightSibling, depth);
+  if (Tree[nodeId].leftChild != NIL) setDepth(Tree[nodeId].leftChild, depth + 1);
+}
+
+int main() {
+  int id, childNum, childId;
+  int leftChildId;
+  cin >> n;
+  rep(i, n) Tree[i].parent = Tree[i].leftChild = Tree[i].rightSibling = NIL;
+
+  rep(i, n) {
+    cin >> id >> childNum;
+
+    rep(i, childNum) {
+      if (childNum != 0) {
+        cin >> childId;
+
+        if (i == 0) {
+          Tree[id].leftChild = childId;
+        }
+        else {
+          Tree[leftChildId].rightSibling = childId;
+        }
+
+        leftChildId = childId;
+
+        Tree[childId].parent = id;
+      }
+    }
+  }
+  
+
+  int rootId;
+
+  rep(i, n) {
+    if (Tree[i].parent == NIL) rootId = i;
+  }
+
+  setDepth(rootId, 0);
+
+  // rep(i, n) cout << Depth[i];
+
+  rep(i, n) printNode(i);
+
+  return 0;
+}
