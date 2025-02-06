55e5e6c590767fd83f24bd6e710f0773cc8076c9
WNomunomu <kohsei.nmr@gmail.com>
2025-02-07 00:39:33 +0900

    binary search

--- 5.search/binary_search.cpp
+++ 5.search/binary_search.cpp
@@ -1,41 +1,46 @@
-#include<stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-// ng と ok の境界線を求めるアルゴリズム
-int binarySearch(int key, int S[], int n) {
-  int ng = -1;
-  int ok = n;
+#define rep(i, n) for (int i = 0; i < n; i++)
 
-  // 境界線が求まるまで続ける
-  while (ok - ng > 1) {
-    int mid = (ng + ok) / 2;
-    if (S[mid] == key) {
-      return 1;
+int n;
+int count = 0;
+
+void binarySearch(int S[], int x) {
+  int length = n;
+  int left = 0;
+  int right = n;
+  int mid;
+
+  while (left < right) {
+    int mid = (right + left) / 2;
+    if (x == S[mid]) {
+      count++;
+      return;
     }
-    else if (S[mid] < key) {
-      ng = mid;
+    else if (x > S[mid]) {
+      left = mid + 1;
     }
-    else if (S[mid] > key) {
-      ok = mid;
+    else {
+      right = mid;
     }
   }
-
-  return 0;
-}
+};
 
 int main() {
-  int n, q;
-  int sum = 0;
-
-  scanf("%d", &n);
+  int q;
+  cin >> n;
   int S[n];
-  for (int i=0; i<n; i++) scanf("%d", &S[i]);
-  scanf("%d", &q);
-  int T[q];
-  for (int i=0; i<q; i++) scanf("%d", &T[i]);
-
-  for (int i=0; i<q; i++) {
-    if (binarySearch(T[i], S, n)) sum++;
+  rep(i, n) cin >> S[i];
+  cin >> q;
+  int t;
+  rep(i, q) {
+    cin >> t;
+    binarySearch(S, t);
   }
 
-  printf("%d\n", sum);
+  cout << count << endl;
+
+  return 0;
 }
