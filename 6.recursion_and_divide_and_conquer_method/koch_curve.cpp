b27c2cf9be908c6e0e92160282351f78e7565f20
WNomunomu <kohsei.nmr@gmail.com>
2025-02-07 02:23:00 +0900

    koch curve

--- 6.recursion_and_divide_and_conquer_method/koch_curve.cpp
+++ 6.recursion_and_divide_and_conquer_method/koch_curve.cpp
@@ -1,42 +1,44 @@
-#include<stdio.h>
-#include<math.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
 
-struct Point { double x, y; };
+#define rep(i, n) for (int i = 0; i < n; i++)
 
-void draw(int n, Point p1, Point p2) {
-  if (n == 0) return;
-  
-  double theta = M_PI * 60.0 / 180.0;
+int n;
+double theta = M_PI * 60 / 180;
 
-  Point s, t, u;
-  s.x = (p2.x + p1.x * 2) / 3;
-  s.y = (p2.y + p1.y * 2) / 3;
-  t.x = (p2.x * 2 + p1.x) / 3;
-  t.y = (p2.y * 2 + p1.y) / 3;
-  u.x = s.x + (t.x - s.x) * cos(theta) - (t.y - s.y) * sin(theta);
-  u.y = s.y + (t.y - s.y) * cos(theta) + (t.x - s.x) * sin(theta);
+struct Vector {
+  double x;
+  double y;
+};
 
-  draw(n - 1, p1, s);
+void koch_curve(Vector p1, Vector p2, int count) {
+  if (count == 0) return;
+
+  Vector s = { x: (p2.x + p1.x * 2) / 3, y: (p2.y + p1.y * 2) / 3 };
+  Vector t = { x: (p2.x * 2 + p1.x) / 3, y: (p2.y * 2 + p1.y) / 3 };
+  Vector u = {
+    x: s.x + cos(theta) * (t.x - s.x) - sin(theta) * (t.y - s.y),
+    y: s.y + sin(theta) * (t.x - s.x) + cos(theta) * (t.y - s.y),
+  };
+
+  koch_curve(p1, s, count - 1);
   printf("%.8f %.8f\n", s.x, s.y);
-  draw(n - 1, s, u);
+  koch_curve(s, u, count - 1);
   printf("%.8f %.8f\n", u.x, u.y);
-  draw(n - 1, u, t);
+  koch_curve(u, t, count - 1);
   printf("%.8f %.8f\n", t.x, t.y);
-  draw(n - 1, t, p2);
+  koch_curve(t, p2, count - 1);
 }
 
 int main() {
-  int n;
   scanf("%d", &n);
-
-  Point p1, p2;
-  p1.x = 0;
-  p1.y = 0;
-  p2.x = 100;
-  p2.y = 0;
+  Vector p1 = { x: 0, y: 0 };
+  Vector p2 = { x: 100, y: 0 };
 
   printf("%.8f %.8f\n", p1.x, p1.y);
-  draw(n, p1, p2);
+  koch_curve(p1, p2, n);
   printf("%.8f %.8f\n", p2.x, p2.y);
+
+  return 0;
 }
