55425bdc92fc17a72af5acf538006cbffd73fe46
WNomunomu <kohsei.nmr@gmail.com>
2025-02-07 00:39:39 +0900

    hash

--- 5.search/hash.cpp
+++ 5.search/hash.cpp
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<string.h>
 
-#define M 1046527
-#define L 14
+#define rep(i, n) for (int i = 0; i < n; i++)
 
-int n;
+static const int M = 1046527;
+static const int L = 14;
+
 char H[M][L];
 
-int getChar(char ch) {
+int convertChar(char ch) {
   if (ch == 'A') return 1;
   else if (ch == 'C') return 2;
   else if (ch == 'G') return 3;
@@ -15,10 +16,10 @@ int getChar(char ch) {
   else return 0;
 }
 
-int getKey(char str[]) {
-  int sum = 0, p = 1;
-  for (int i=0; i<strlen(str); i++) {
-    sum += p*getChar(str[i]);
+long long getKey(char str[]) {
+  long long sum = 0, p = 1;
+  rep(i, strlen(str)) {
+    sum += p * (convertChar(str[i]));
     p *= 5;
   }
   return sum;
@@ -27,63 +28,57 @@ int getKey(char str[]) {
 int h1(int key) { return key % M; }
 int h2(int key) { return 1 + (key % (M - 1)); }
 
-int insert(char str[]) {
-  int hashKey;
-  int key = getKey(str);
-
-  for (int i=0; ; i++) {
-    hashKey = (h1(key) + i*h2(key)) % M;
-
-    if (strlen(H[hashKey]) == 0) {
-      strcpy(H[hashKey], str);
-      return 0;
+void insert(char str[]) {
+  long long key, h;
+  key = getKey(str);
+  int i = 0;
+  for (i = 0; ; i++) {
+    h = (h1(key) + i * h2(key)) % M;
+    if (strlen(H[h]) == 0) {
+      strcpy(H[h], str);
+      return;
     }
   }
-
-  return 0;
 }
 
-int find(char str[]) {
-  int hashKey;
+bool find(char str[]) {
+  int h;
   int key = getKey(str);
 
-  for (int i=0; ; i++) {
-    hashKey = (h1(key) + i*h2(key)) % M;
+  for(int i = 0; ; i++) {
+    h = (h1(key) + i * h2(key)) % M;
 
     // if (H[hashKey] == str)
     // これだと H[hashKey] の文字列と str が同じアドレスを指しているかを判定してしまう。
-    if (strcmp(H[hashKey], str) == 0) {
-      return 1;
-    }
-    // これ以上探す範囲がなくなったら終了
-    else if (strlen(H[hashKey]) == 0) return 0;
+    if (strcmp(H[h], str) == 0) return true;
+    // そこから先もすべて 0 つまり、空
+    else if (strlen(H[h]) == 0) return false;
   }
 
-  return 0;
+  return false;
 }
 
 int main() {
   int i, n, h;
-  char str[14], com[9];
-  
-  for (i = 0; i < M; i++) H[i][0] = '\0';
+  char str[14], operation[9];
+
+  rep(i, M) {
+    H[i][0] = '\0';
+  }
 
   scanf("%d", &n);
 
-  for (i = 0; i < n; i++) {
-    scanf("%s %s", com, str);
+  rep(i, n) {
+    scanf("%s %s", operation, str);
 
-    if (com[0] == 'i') {
+    if (operation[0] == 'i') {
       insert(str);
     }
     else {
-      if (find(str)) {
-        printf("yes\n");
-      }
-      else {
-        printf("no\n");
-      }
+      if (find(str)) printf("yes\n");
+      else printf("no\n");
     }
   }
+
   return 0;
 }
